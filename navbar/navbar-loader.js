71b102f6e7307c26b002c35fe9ec68afc55aec73
senppie <pedromoreiraa2002@gmail.com>
2026-04-09 14:57:42 +0100

    UI changes on Homepage

--- navbar/navbar-loader.js
+++ navbar/navbar-loader.js
@@ -1,6 +1,6 @@
 async function loadNavbar() {
   try {
-    const response = await fetch('assets/navbar.html');
+    const response = await fetch('navbar/navbar.html');
     const navbarHTML = await response.text();
     document.body.insertAdjacentHTML('afterbegin', navbarHTML);
   } catch (error) {
